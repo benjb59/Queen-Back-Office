fd4996a2da99de543c345b80506342aa18c8a8e8
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-07-16 12:38:17 +0200

    deposit-proof : init

--- src/main/java/fr/insee/queen/api/pdfutils/FoToPDFTransformation.java
+++ src/main/java/fr/insee/queen/api/pdfutils/FoToPDFTransformation.java
@@ -1,8 +1,16 @@
 package fr.insee.queen.api.pdfutils;
 
 import org.apache.fop.apps.*;
+import org.apache.fop.apps.io.InternalResourceResolver;
+import org.apache.fop.configuration.Configuration;
+import org.apache.fop.fonts.FontManager;
+import org.apache.fop.layoutmgr.LayoutManagerMaker;
+import org.apache.xmlgraphics.image.loader.ImageManager;
+import org.apache.xmlgraphics.image.loader.impl.AbstractImageSessionContext;
+import org.apache.xmlgraphics.io.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -12,20 +20,42 @@ import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
 
 public class FoToPDFTransformation {
     static Logger LOGGER = LoggerFactory.getLogger(FoToPDFTransformation.class);
 
+    private FopFactory fopFactory;
+    private FOUserAgent foUserAgent;
+
+    public FoToPDFTransformation() {
+        InputStream isXconf = Constants.getInputStreamFromPath(Constants.FOP_CONF);
+        URI folderBase = null;
+        try {
+            folderBase = FoToPDFTransformation.class.getResource("/pdf/").toURI();
+            LOGGER.info("URI folderBase : "+folderBase);
+            FopFactory fopFactory = FopFactory.newInstance(folderBase, isXconf);
+            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+            this.fopFactory = fopFactory;
+            this.foUserAgent = foUserAgent;
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
     public File transformFoToPdf(File foFile) throws Exception {
         File outFilePDF = File.createTempFile("pdf-file",".pdf");
         try{
-            InputStream isXconf = Constants.getInputStreamFromPath(Constants.FOP_CONF);
-            URI folderBase = FoToPDFTransformation.class.getResource("/pdf/img/").toURI();
-            LOGGER.info("Uri folderBase :"+folderBase);
-            FopFactory fopFactory = FopFactory.newInstance(folderBase,isXconf);
-            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
             OutputStream out = new BufferedOutputStream(new FileOutputStream(outFilePDF));
-            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF,foUserAgent, out);
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer();
             Source src = new StreamSource(foFile);
